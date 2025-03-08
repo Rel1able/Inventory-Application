4201b46aa846698797385bc75cc8f389ce426696
Yaroslav <yaroslav.shebovovkk@gmail.com>
2025-03-08 22:04:24 +0100

    created start db script

--- db/populatedb.js
+++ db/populatedb.js
@@ -5,6 +5,10 @@ const { argv } = require("node:process");
 console.log(argv);
 
 const SQL = `
+DROP TABLE categories;
+DROP TABLE food;
+DROP TABLE suppliers;
+
 CREATE TABLE IF NOT EXISTS categories (
  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
  category_name VARCHAR(255));
@@ -14,6 +18,30 @@ INSERT INTO categories (category_name)
     ('dairy'),
     ('muka'),
     ('fruits');
+
+CREATE TABLE IF NOT EXISTS food (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    food_name VARCHAR(255),
+    category_id INTEGER,
+    supplier_id INTEGER
+);
+
+INSERT INTO food (food_name, category_id, supplier_id)
+    VALUES
+        ('milk',1, 2),
+        ('apple',3, 3),
+        ('banana', 3, 4);
+
+CREATE TABLE IF NOT EXISTS suppliers (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    supplier_name VARCHAR(255));
+
+INSERT INTO suppliers (supplier_name)
+    VALUES
+        ('Vevela'),
+        ('Lovea'),
+        ('Serilia');
+
 `;
 
 
@@ -28,3 +56,5 @@ async function main() {
     console.log("done");
 }
 main();
+
+
