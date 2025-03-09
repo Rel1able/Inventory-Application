d31b7f805a700e8a291a12b76cddd3bd43b2f55b
Yaroslav <yaroslav.shebovovkk@gmail.com>
2025-03-09 08:52:34 +0100

    fulfilled populateddb query with values

--- db/populatedb.js
+++ db/populatedb.js
@@ -5,9 +5,7 @@ const { argv } = require("node:process");
 console.log(argv);
 
 const SQL = `
-DROP TABLE categories;
-DROP TABLE food;
-DROP TABLE suppliers;
+
 
 CREATE TABLE IF NOT EXISTS categories (
  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
@@ -15,9 +13,11 @@ CREATE TABLE IF NOT EXISTS categories (
 
 INSERT INTO categories (category_name)
  VALUES
-    ('dairy'),
-    ('muka'),
-    ('fruits');
+    ('Fruits'),
+    ('Vegetables'),
+    ('Grains'),
+    ('Protein Food'),
+    ('Dairy');
 
 CREATE TABLE IF NOT EXISTS food (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
@@ -28,9 +28,41 @@ CREATE TABLE IF NOT EXISTS food (
 
 INSERT INTO food (food_name, category_id, supplier_id)
     VALUES
-        ('milk',1, 2),
-        ('apple',3, 3),
-        ('banana', 3, 4);
+        ('apple', 1, 1),
+        ('banana', 1, 1),
+        ('orange', 1, 1),
+        ('peach', 1, 1),
+        ('pear', 1, 1),
+        ('grapes', 1, 1),
+        ('watermelon', 1, 1),
+        ('potatoes', 2, 2),
+        ('carrots', 2, 2),
+        ('mushrooms', 2, 2),
+        ('broccoli', 2, 2),
+        ('tomatoes', 2, 2),
+        ('corn', 2, 2),
+        ('avocado', 2, 2),
+        ('bread', 3, 3),
+        ('rice', 3, 3),
+        ('popcorn', 3, 3),
+        ('oatmeal', 3, 3),
+        ('pasta', 3, 3),
+        ('pretzels', 3, 3),
+        ('muffins', 3, 3),
+        ('eggs', 4, 4),
+        ('beef', 4, 4),
+        ('chicken', 4, 4),
+        ('nuts', 4, 4),
+        ('pork', 4, 4),
+        ('salmon', 4, 4),
+        ('tuna', 4, 4),
+        ('milk', 5, 5),
+        ('yogurt', 5, 5),
+        ('kefir', 5, 5),
+        ('cheese', 5, 5),
+        ('soymilk', 5, 5),
+        ('butter', 5, 5),
+        ('ice cream', 5, 5);
 
 CREATE TABLE IF NOT EXISTS suppliers (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
@@ -38,9 +70,11 @@ CREATE TABLE IF NOT EXISTS suppliers (
 
 INSERT INTO suppliers (supplier_name)
     VALUES
-        ('Vevela'),
-        ('Lovea'),
-        ('Serilia');
+        ('Dole Food Company'),
+        ('Fresh Del Monte'),
+        ('Cargil'),
+        ('Tyson Foods'),
+        ('Lactalis');
 
 `;
 
