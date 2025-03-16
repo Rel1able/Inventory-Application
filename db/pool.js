e49c9b0836d1bad75705878f30c73c7353bffccb
Yaroslav <yaroslav.shebovovkk@gmail.com>
2025-03-16 20:55:28 +0100

    fixed db access data

--- db/pool.js
+++ db/pool.js
@@ -2,9 +2,6 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 module.exports = new Pool({
-    host: "localhost",
-    user: process.env.USERNAME,
-    database: "inventory_app",
-    password: process.env.USER_PSW,
-    port: 5432
+    connectionString: process.env.CONNECTION_STRING
 });
+
